78b17bf7f6dbeea7d2c71f2d945759e64abad0f9
Pirmin <pirmin.aster@mci4me.at>
2022-02-28 18:20:32 +0100

    test

---
+++ cmake-build-debug/CMakeFiles/2_Aufgabe.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "2_Aufgabe"
+  "2_Aufgabe.pdb"
+  "CMakeFiles/2_Aufgabe.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2_Aufgabe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
